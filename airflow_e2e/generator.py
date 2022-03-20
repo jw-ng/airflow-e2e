3efdb455cbd543819e94a69e5fbc34dccc2f9506
Jun Wei <ngjunwei.15@gmail.com>
2022-03-20 10:39:08 +0800

    Support generation of docker-compose-tests.yml

--- airflow_e2e/generator.py
+++ airflow_e2e/generator.py
@@ -1,30 +1,61 @@
 from pathlib import Path
+from string import Template
 
 DOCKER_FOLDER_NAME = "docker"
 
 REPO_ROOT_DIR_PATH = Path(__file__).resolve().parent.parent
 TEMPLATES_FOLDER_NAME = "templates"
 
+DAGS_FOLDER_TEMPLATE_STRING = "DAGS_FOLDER"
+TESTS_FOLDER_TEMPLATE_STRING = "TESTS_FOLDER"
+
 DOCKER_COMPOSE_YML_FILE_NAME = "docker-compose.yml"
 DOCKER_COMPOSE_YML_TEMPLATE_FILE_NAME = f"{DOCKER_COMPOSE_YML_FILE_NAME}.template"
+
+DOCKER_COMPOSE_TESTS_YML_FILE_NAME = "docker-compose-tests.yml"
+DOCKER_COMPOSE_TESTS_YML_TEMPLATE_FILE_NAME = (
+    f"{DOCKER_COMPOSE_TESTS_YML_FILE_NAME}.template"
+)
 
 
 def generate(
     dags: str,
+    tests: str,
     working_dir: str,
 ):
+    substitutions = {
+        DAGS_FOLDER_TEMPLATE_STRING: dags,
+        TESTS_FOLDER_TEMPLATE_STRING: tests,
+    }
+
     docker_folder_path = Path(working_dir) / DOCKER_FOLDER_NAME
 
     docker_folder_path.mkdir(parents=True)
 
-    template_yml_file_path = (
+    docker_compose_yml_template_file_path = (
         REPO_ROOT_DIR_PATH
         / TEMPLATES_FOLDER_NAME
         / DOCKER_COMPOSE_YML_TEMPLATE_FILE_NAME
     )
-    with template_yml_file_path.open(mode="r") as template_file:
-        template = template_file.read()
+    with docker_compose_yml_template_file_path.open(mode="r") as template_file:
+        docker_compose_yml_template = Template(template_file.read())
 
     docker_compose_yml_file_path = docker_folder_path / DOCKER_COMPOSE_YML_FILE_NAME
     with docker_compose_yml_file_path.open("w") as docker_compose_yml_file:
-        docker_compose_yml_file.write(template.replace("{{ DAGS_FOLDER }}", dags))
+        content = docker_compose_yml_template.substitute(**substitutions)
+        docker_compose_yml_file.write(content)
+
+    docker_compose_tests_yml_template_file_path = (
+        REPO_ROOT_DIR_PATH
+        / TEMPLATES_FOLDER_NAME
+        / DOCKER_COMPOSE_TESTS_YML_TEMPLATE_FILE_NAME
+    )
+    with docker_compose_tests_yml_template_file_path.open(mode="r") as template_file:
+        docker_compose_tests_yml_template = Template(template_file.read())
+
+    docker_compose_tests_yml_file_name = (
+        docker_folder_path / DOCKER_COMPOSE_TESTS_YML_FILE_NAME
+    )
+    with docker_compose_tests_yml_file_name.open("w") as docker_compose_tests_yml_file:
+        content = docker_compose_tests_yml_template.substitute(**substitutions)
+        docker_compose_tests_yml_file.write(content)
--- tests/airflow_e2e/test_generator.py
+++ tests/airflow_e2e/test_generator.py
@@ -8,6 +8,7 @@ def test_should_create_docker_base_folder():
     with tempfile.TemporaryDirectory() as temp_dir:
         generate(
             dags="some/dags/folder",
+            tests="some/tests/folder",
             working_dir=temp_dir,
         )
 
@@ -20,6 +21,7 @@ def test_should_create_docker_compose_yml_file_in_docker_base_folder():
     with tempfile.TemporaryDirectory() as temp_dir:
         generate(
             dags="some/dags/folder",
+            tests="some/tests/folder",
             working_dir=temp_dir,
         )
 
@@ -32,6 +34,7 @@ def test_should_setup_correct_dags_folder_in_docker_compose_yml_file():
     with tempfile.TemporaryDirectory() as temp_dir:
         generate(
             dags="some/dags/folder",
+            tests="some/tests/folder",
             working_dir=temp_dir,
         )
 
@@ -42,6 +45,39 @@ def test_should_setup_correct_dags_folder_in_docker_compose_yml_file():
 
         expected_docker_compose_yml_file_path = Path(__file__).resolve().parent / "resources" / "expected_docker_compose.yml"
         with expected_docker_compose_yml_file_path.open() as f:
+            expected = f.read()
+
+        assert actual == expected
+
+
+def test_should_create_docker_compose_tests_yml_file_in_docker_base_folder():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        docker_compose_tests_yml_file_path = Path(temp_dir) / "docker" / "docker-compose-tests.yml"
+
+        assert docker_compose_tests_yml_file_path.exists()
+
+
+def test_should_setup_correct_dags_folder_in_docker_compose_tests_yml_file():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        docker_compose_tests_yml_file_path = Path(temp_dir) / "docker" / "docker-compose-tests.yml"
+
+        with docker_compose_tests_yml_file_path.open() as f:
+            actual = f.read()
+
+        expected_docker_compose_tests_yml_file_path = Path(__file__).resolve().parent / "resources" / "expected_docker-compose-tests.yml"
+        with expected_docker_compose_tests_yml_file_path.open() as f:
             expected = f.read()
 
         assert actual == expected
