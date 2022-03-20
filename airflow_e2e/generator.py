e6cf19188f99ebf7ef12926c5721e38d7ab16e94
Jun Wei <ngjunwei.15@gmail.com>
2022-03-20 11:17:19 +0800

    Support generation of docker-compose-dev.yml and airflow conns and vars seeder setup

--- airflow_e2e/generator.py
+++ airflow_e2e/generator.py
@@ -18,10 +18,26 @@ DOCKER_COMPOSE_TESTS_YML_TEMPLATE_FILE_NAME = (
     f"{DOCKER_COMPOSE_TESTS_YML_FILE_NAME}.template"
 )
 
+DOCKER_COMPOSE_DEV_YML_FILE_NAME = "docker-compose-dev.yml"
+DOCKER_COMPOSE_DEV_YML_TEMPLATE_FILE_NAME = (
+    f"{DOCKER_COMPOSE_DEV_YML_FILE_NAME}.template"
+)
+
 TEMPLATE_MAP = {
     DOCKER_COMPOSE_YML_TEMPLATE_FILE_NAME: DOCKER_COMPOSE_YML_FILE_NAME,
     DOCKER_COMPOSE_TESTS_YML_TEMPLATE_FILE_NAME: DOCKER_COMPOSE_TESTS_YML_FILE_NAME,
+    DOCKER_COMPOSE_DEV_YML_TEMPLATE_FILE_NAME: DOCKER_COMPOSE_DEV_YML_FILE_NAME,
 }
+
+AIRFLOW_CONNECTIONS_AND_VARIABLES_SEEDER_FOLDER_NAME = (
+    "airflow-connections-and-variables-seeder"
+)
+
+CONNECTIONS_YML_TEMPLATE_FILE_NAME = "connections.yml.template"
+CONNECTIONS_YML_FILE_NAME = "connections.yml"
+
+VARIABLES_JSON_TEMPLATE_FILE_NAME = "variables.json.template"
+VARIABLES_JSON_FILE_NAME = "variables.json"
 
 
 def generate(dags: str, tests: str, working_dir: str):
@@ -41,6 +57,10 @@ def generate(dags: str, tests: str, working_dir: str):
             substitutions=substitutions,
         )
 
+    _setup_airflow_connections_and_variables_seeder_folder(
+        docker_folder_path=docker_folder_path
+    )
+
 
 def _setup_docker_compose_file(
     template_file_name: str,
@@ -58,3 +78,41 @@ def _setup_docker_compose_file(
     with docker_compose_yml_file_path.open("w") as docker_compose_yml_file:
         content = docker_compose_yml_template.substitute(**substitutions)
         docker_compose_yml_file.write(content)
+
+
+def _setup_airflow_connections_and_variables_seeder_folder(docker_folder_path: Path):
+    airflow_connections_and_variables_seeder_folder_path = (
+        docker_folder_path / AIRFLOW_CONNECTIONS_AND_VARIABLES_SEEDER_FOLDER_NAME
+    )
+    airflow_connections_and_variables_seeder_folder_path.mkdir(parents=True)
+
+    seeder_template_map = {
+        CONNECTIONS_YML_TEMPLATE_FILE_NAME: CONNECTIONS_YML_FILE_NAME,
+        VARIABLES_JSON_TEMPLATE_FILE_NAME: VARIABLES_JSON_FILE_NAME,
+    }
+
+    for template_file_name, output_file_name in seeder_template_map.items():
+        _create_seeder_template_file(
+            template_file_name=template_file_name,
+            output_file_name=output_file_name,
+            seeder_base_folder_path=airflow_connections_and_variables_seeder_folder_path,
+        )
+
+
+def _create_seeder_template_file(
+        template_file_name: str,
+        output_file_name: str,
+        seeder_base_folder_path: Path,
+):
+    seeder_template_file_path = (
+            REPO_ROOT_DIR_PATH
+            / TEMPLATES_FOLDER_NAME
+            / AIRFLOW_CONNECTIONS_AND_VARIABLES_SEEDER_FOLDER_NAME
+            / template_file_name
+    )
+    with seeder_template_file_path.open(mode="r") as template_file:
+        template = template_file.read()
+
+    output_file_path = seeder_base_folder_path / output_file_name
+    with output_file_path.open(mode="w") as f:
+        f.write(template)
--- tests/airflow_e2e/test_generator.py
+++ tests/airflow_e2e/test_generator.py
@@ -1,3 +1,4 @@
+import json
 import tempfile
 from pathlib import Path
 
@@ -43,7 +44,11 @@ def test_should_setup_correct_dags_folder_in_docker_compose_yml_file():
         with docker_compose_yml_file_path.open() as f:
             actual = f.read()
 
-        expected_docker_compose_yml_file_path = Path(__file__).resolve().parent / "resources" / "expected_docker_compose.yml"
+        expected_docker_compose_yml_file_path = (
+            Path(__file__).resolve().parent
+            / "resources"
+            / "expected_docker_compose.yml"
+        )
         with expected_docker_compose_yml_file_path.open() as f:
             expected = f.read()
 
@@ -58,7 +63,9 @@ def test_should_create_docker_compose_tests_yml_file_in_docker_base_folder():
             working_dir=temp_dir,
         )
 
-        docker_compose_tests_yml_file_path = Path(temp_dir) / "docker" / "docker-compose-tests.yml"
+        docker_compose_tests_yml_file_path = (
+            Path(temp_dir) / "docker" / "docker-compose-tests.yml"
+        )
 
         assert docker_compose_tests_yml_file_path.exists()
 
@@ -71,13 +78,162 @@ def test_should_setup_correct_dags_folder_in_docker_compose_tests_yml_file():
             working_dir=temp_dir,
         )
 
-        docker_compose_tests_yml_file_path = Path(temp_dir) / "docker" / "docker-compose-tests.yml"
+        docker_compose_tests_yml_file_path = (
+            Path(temp_dir) / "docker" / "docker-compose-tests.yml"
+        )
 
         with docker_compose_tests_yml_file_path.open() as f:
             actual = f.read()
 
-        expected_docker_compose_tests_yml_file_path = Path(__file__).resolve().parent / "resources" / "expected_docker-compose-tests.yml"
+        expected_docker_compose_tests_yml_file_path = (
+            Path(__file__).resolve().parent
+            / "resources"
+            / "expected_docker-compose-tests.yml"
+        )
         with expected_docker_compose_tests_yml_file_path.open() as f:
             expected = f.read()
 
         assert actual == expected
+
+
+def test_should_create_docker_compose_dev_yml_file_in_docker_base_folder():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        docker_compose_dev_yml_file_path = (
+            Path(temp_dir) / "docker" / "docker-compose-dev.yml"
+        )
+
+        assert docker_compose_dev_yml_file_path.exists()
+
+
+def test_should_setup_correct_dags_folder_in_docker_compose_dev_yml_file():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        docker_compose_dev_yml_file_path = (
+            Path(temp_dir) / "docker" / "docker-compose-dev.yml"
+        )
+
+        with docker_compose_dev_yml_file_path.open() as f:
+            actual = f.read()
+
+        expected_docker_compose_dev_yml_file_path = (
+            Path(__file__).resolve().parent
+            / "resources"
+            / "expected_docker-compose-dev.yml"
+        )
+        with expected_docker_compose_dev_yml_file_path.open() as f:
+            expected = f.read()
+
+        assert actual == expected
+
+
+def test_should_create_airflow_connections_and_variable_seeder_folder_in_docker_base_folder():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        expected_seeder_folder_path = (
+            Path(temp_dir) / "docker" / "airflow-connections-and-variables-seeder"
+        )
+
+        assert expected_seeder_folder_path.exists()
+
+
+def test_should_create_connections_seeder_yml_file_in_airflow_connections_and_variables_seeder_folder():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        connections_seeder_yml_file = (
+            Path(temp_dir)
+            / "docker"
+            / "airflow-connections-and-variables-seeder"
+            / "connections.yml"
+        )
+
+        assert connections_seeder_yml_file.exists()
+
+
+def test_should_write_some_example_connections_in_connections_seeder_yml_file():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        connections_seeder_yml_file = (
+            Path(temp_dir)
+            / "docker"
+            / "airflow-connections-and-variables-seeder"
+            / "connections.yml"
+        )
+
+        with connections_seeder_yml_file.open() as f:
+            actual = f.readlines()
+
+        assert actual == [
+            "example_conn_id:\n",
+            "  conn_type: mongo\n",
+            "  host: 192.168.1.123\n",
+            "  port: 27017\n",
+        ]
+
+
+def test_should_create_variables_json_file_in_airflow_connections_and_variables_seeder_folder():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        variables_json_file = (
+            Path(temp_dir)
+            / "docker"
+            / "airflow-connections-and-variables-seeder"
+            / "variables.json"
+        )
+
+        assert variables_json_file.exists()
+
+
+def test_should_write_some_example_variables_in_variables_json_file():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        generate(
+            dags="some/dags/folder",
+            tests="some/tests/folder",
+            working_dir=temp_dir,
+        )
+
+        variables_json_file = (
+            Path(temp_dir)
+            / "docker"
+            / "airflow-connections-and-variables-seeder"
+            / "variables.json"
+        )
+
+        with variables_json_file.open() as f:
+            actual = json.load(f)
+
+        assert actual == {
+            "example_string_variable": "example_string_value",
+            "example_json_variable": {"foo": "bar", "baz": 42},
+            "example_array_variable": ["lorem", "ipsum"],
+        }
