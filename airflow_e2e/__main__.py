97c8df14eaf961f65a8e5a18df74c63d1abdb70b
Jun Wei <ngjunwei.15@gmail.com>
2022-03-21 13:19:49 +0800

    Generate e2e test scripts

--- airflow_e2e/__main__.py
+++ airflow_e2e/__main__.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from airflow_e2e import parser
@@ -5,6 +6,9 @@ from airflow_e2e import parser
 
 def main():
     args = parser.parse(sys.argv[1:])
+
+    working_dir = os.getcwd()
+    generate(dags=args.dags, tests=args.tests, working_dir=working_dir)
 
 
 if __name__ == "__main__":
