d9f2142001e07c03dc8603d4af29f0984776ba79
Jun Wei <ngjunwei.15@gmail.com>
2022-03-22 10:58:11 +0800

    Add help message for arguments

--- airflow_e2e/parser.py
+++ airflow_e2e/parser.py
@@ -4,7 +4,15 @@ import typing
 
 def parse(args: typing.List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dags", required=True)
-    parser.add_argument("--tests", required=True)
+    parser.add_argument(
+        "--dags",
+        required=True,
+        help="DAGs folder path, relative from the root of the repository",
+    )
+    parser.add_argument(
+        "--tests",
+        required=True,
+        help="E2E test suite folder path, relative from the root of the repository",
+    )
 
     return parser.parse_args(args)
